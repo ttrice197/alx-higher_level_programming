87bd52a0ec07fee984a7b3d27389fd24a5d8827d
ttrice197 <118218013+ttrice197@users.noreply.github.com>
2023-03-18 05:19:17 +0000

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = dict(map(lambda x: (x[0], x[1] * 2), a_dictionary.items()))
-    return (new_dict)
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+
+    for i in list_keys:
+        new_dir[i] *= 2
+
+    return (new_dir)
+
