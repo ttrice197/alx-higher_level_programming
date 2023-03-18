d604560804c06617b03d012b783869a20691f7c9
ttrice197 <118218013+ttrice197@users.noreply.github.com>
2023-03-18 05:10:26 +0000

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sm = 0
-    list_uq = list(dict.fromkeys(my_list))
-    for i in range(len(list_uq)):
-        sm += list_uq[i]
-    return (sm)    
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
 
 
